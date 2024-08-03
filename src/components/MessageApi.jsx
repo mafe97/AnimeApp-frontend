d41eb2cf2228fe6b371c93c005bff69f319dfc56
mafe97 <mf972107@gmail.com>
2024-08-03 09:02:35 -0500

    Add new component MessageApi

---
+++ src/components/MessageApi.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+
+import axios from "axios";
+
+import { AprUrl } from "../contexts/ids";
+
+export const MessageApi = () => {
+    const [isLoading, setIsLoading] = useState(true);
+
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+      const fetchAverageScore = async () => {
+        try {
+          const response = await axios.get(`${AprUrl}/score`);
+          setMessage(response.data);
+          setIsLoading(false);
+        } catch (error) {
+          console.error('Error fetching average score:', error);
+        }
+      };
+  
+      fetchAverageScore();
+    }, []);
+  
+    return (
+
+        isLoading,
+        <div>
+            <p>This Anime is { message }</p>
+      </div>
+    );
+}
