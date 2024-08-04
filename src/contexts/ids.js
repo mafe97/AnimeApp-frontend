3c3bb5a219bd92e411f3bcfd31aade1907a9cda0
mafe97 <mf972107@gmail.com>
2024-08-03 23:31:04 -0500

    Update ids file

--- src/contexts/ids.js
+++ src/contexts/ids.js
@@ -3,6 +3,6 @@
 // ------------------------------------------------ //
 
 // API local
-export const ApiUrl = 'https://34.236.248.124';
+export const ApiUrl = 'https://api.jikan.moe';
 // API production
 export const AprUrl = 'https://34.236.248.124/v4/anime';
